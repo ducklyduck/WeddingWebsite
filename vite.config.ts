3bf484ff1fca3a2de42dfc6e639c032d05ead8ec
ducklyduck <101879491+ducklyduck@users.noreply.github.com>
2025-07-03 18:28:00 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/WeddingWebsite/', // добавьте эту строку!
+  // base: '/WeddingWebsite/', // добавьте эту строку!
   plugins: [react()],
   css: {
     preprocessorOptions: {
