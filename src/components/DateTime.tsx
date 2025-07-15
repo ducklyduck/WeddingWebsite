c18c2d1e030f8195c305d3b1d6a4b643f3bced52
ducklyduck <101879491+ducklyduck@users.noreply.github.com>
2025-07-15 15:03:06 +0500

    fix7

--- src/components/DateTime.tsx
+++ src/components/DateTime.tsx
@@ -4,13 +4,13 @@ function DateTime() {
 
   return (
     <Typography className='section-dateTime' >
-      <Typography.Title className="section-title" >
+      <Typography.Title className="dateTime-title section-title" >
         Ждём вас
       </Typography.Title>
       <Typography.Paragraph className='dateTime-paragraph section-paragraph' >
         15 августа в 15:30
       </Typography.Paragraph>
-      <Typography.Title className="section-title" >
+      <Typography.Title className="dateTime-title section-title" >
         Август
       </Typography.Title>
       <div className='dateTime-calendar'> </div>
